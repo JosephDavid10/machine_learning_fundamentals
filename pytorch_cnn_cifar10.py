7533eef7c8524c4b515bb41ae065a9d844f89c7e
JosephDavid10 <jose.david.ssma@gmail.com>
2026-05-19 17:01:04 -0300

    v1.2: early stopping and validations set addition

--- pytorch_cnn_cifar10.py
+++ pytorch_cnn_cifar10.py
@@ -13,11 +13,20 @@ transform = v2.Compose([
 
 batch_size = 16
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+trainsetfull = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
+#split in train and validation set
+num_val = 5000
+num_train = len(trainsetfull) - num_val # 45000
+
+generator = torch.Generator().manual_seed(42)
+valset, trainset = random_split(trainsetfull, [num_val, num_train], generator=generator)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=0)
+valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
+                                          shuffle=True, num_workers=0)
 
+# train set
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
@@ -58,32 +67,86 @@ import torch.optim as optim
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# training for 10 epochs
-for epoch in range(10):  # loop over the dataset multiple times
+# training for 50 epochs
+for epoch in range(50):  # loop over the dataset multiple times
 
     running_loss = 0.0
+    train_total = 0
+    train_correct = 0
+    net.train()
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
+    
+        # acceleration with CPU
         inputs = inputs.to(device)
         labels = labels.to(device)
-
+        
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
         outputs = net(inputs)
         loss = criterion(outputs, labels)
+        
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        train_total += labels.size(0)
+        train_correct += (predicted == labels).sum().item()
+
+        epoch_train_loss = running_loss / len(trainloader)
+        epoch_train_acc = 100 * train_correct / train_total
+        
         loss.backward()
         optimizer.step()
+        
+    #validation 
+    net.eval()  # 1. Ativa o modo de avaliação (desativa dropout/batchnorm)
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 3125 == 3124 :    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 3125:.3f}')
-            running_loss = 0.0
+    val_loss = 0.0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():  # 2. Desativa gradientes (Economiza VRAM e acelera o hardware)
+        for data in valloader:  # Dica: rode este loader com o dobro do batch size
+            images, labels = data[0].to(device), data[1].to(device)
+    
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+    
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    # Calcula as métricas finais da época
+    epoch_val_loss = val_loss / len(valloader)
+    epoch_val_acc = 100 * correct / total
+
+    if epoch_val_acc > best_val_acc:
+        best_val_acc = epoch_val_acc
+        # Salva os pesos do melhor momento na GPU para usar depois
+        torch.save(net.state_dict(), 'best_model_cifar10.pth')
+        #print(f"--> Novo melhor modelo salvo com Val Acc: {bestr_val_acc:.2f}%")   
+        patience = 0
+    
+    else:
+        patience += 1
+
+    # print statistics
+    print(f'[{epoch + 1}, {i + 1:5d}] loss: {epoch_train_loss:.3f}, accuracy: {epoch_train_acc:.3f}, val_loss: {epoch_val_loss:.3f}, val_accuracy: {epoch_val_acc:.3f}')
+    running_loss = 0.0
+
+    if patience == 5:
+        break
 
 print('Finished Training')
+
+# Recalling the best model
+best_net = Net()
+weights = torch.load('best_model_cifar10.pth', map_location=device, weights_only = True)
+best_net.load_state_dict(weights)
+best_net = best_net.to(device)
 
 # evaluation of the model
 correct = 0
@@ -97,7 +160,7 @@ with torch.no_grad():
         images = images.to(device)
         labels = labels.to(device)
         
-        outputs = net(images)
+        outputs = best_net(images)
         
         _, predicted = torch.max(outputs.data, 1)
         
@@ -111,14 +174,14 @@ print(f'Accuracy of the network on the 10000 test images: {total_acuracy:.2f}%')
 correct_pred = {classname: 0 for classname in classes}
 total_pred = {classname: 0 for classname in classes}
 
-# again no gradients needed
+# no gradients needed
 with torch.no_grad():
     for data in testloader:
         images, labels = data
         images = images.to(device)
         labels = labels.to(device)
         
-        outputs = net(images)
+        outputs = best_net(images)
         _, predictions = torch.max(outputs, 1)
         # collect the correct predictions for each class
         for label, prediction in zip(labels, predictions):
