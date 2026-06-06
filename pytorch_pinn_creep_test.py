00e934029911d8ca384610cc5a5dbf9c6fef8010
JosephDavid10 <jose.david.ssma@gmail.com>
2026-06-05 23:49:21 -0300

    Inversion model implementation
    Inversion model for parameter estimation.

--- pytorch_pinn_creep_test.py
+++ pytorch_pinn_creep_test.py
@@ -40,3 +40,45 @@ for i in range(1, step):
 epsilon_real = torch.stack(epsilon_list)
 
 plt.plot(t, epsilon_real)
+
+
+#===========================================
+# INVERSION MODEL
+#===========================================
+# estimating initial inputs
+E_inf_est = torch.tensor(25., requires_grad = True)  
+E_i_est = torch.tensor([25., 5., 20.], requires_grad = True)  
+tau_i_est = torch.tensor([10., 15., 20.], requires_grad = False) 
+
+optimizer = torch.optim.Adam([E_inf_est, E_i_est], lr = 0.1) 
+
+# parameter estimation
+epochs = 300
+for epoch in range(epochs):
+    optimizer.zero_grad()
+
+    E_0_est = E_inf_est + torch.sum(E_i_est)
+    epsilon_i_est = sigma_0/E_0_est
+    sigma_i_est = E_i_est * epsilon_i_est
+
+    epsilon_est = torch.zeros(step)
+    epsilon_est[0] = epsilon_i_est 
+    deps_dt_est = 0.0
+    for k in range(1, step):
+        deps_dt_est =  torch.sum(sigma_i_est*(1/tau_i_est))/E_0_est
+        epsilon_i_est = epsilon_i_est + deps_dt_est*dt
+        epsilon_est[k] = epsilon_i_est
+        ds_dt_est = E_i_est * deps_dt_est - (sigma_i_est / tau_i_est)
+        sigma_i_est = sigma_i_est + ds_dt_est * dt
+
+    E_i_formatado = [f"{x:.2f}" for x in E_i_est.tolist()]
+        
+    # loss function
+    loss = torch.mean((epsilon_real - epsilon_est)**2)
+
+    loss.backward()
+    optimizer.step()
+    
+     # Exibe o progresso a cada 50 iterações
+    if epoch % 50 == 0 or epoch == epochs - 1:
+        print(f"Época {epoch:03d} | Erro: {loss.item():.6f} | E_inf: {E_inf_est.item():.2f} | E_i: {E_i_formatado}")
